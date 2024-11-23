971196c2517c1be851dd8ac733bf3b9d500e7627
Shahla-AAFS <139673365+Shahla-AAFS@users.noreply.github.com>
2024-11-23 13:41:53 +0000

    changed some lines

--- assignment04/src/App.js
+++ assignment04/src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import AnimalTable from './AnimalTable';
+
 
 function App() {
   return (
     <div className="App">
-      <AnimalTable/>
+      <AnimalCard/>
     </div>
   );
 }
