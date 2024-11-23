4a2a453ba75c35abc3b06f7bbcff752df42f9f43
Shahla-AAFS <139673365+Shahla-AAFS@users.noreply.github.com>
2024-11-23 12:21:06 +0000

    cont

--- assignment04/src/App.js
+++ assignment04/src/App.js
@@ -1,39 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{useState} from 'react'
-import AnimalCard from './AnimalCard';
-import animals from './AnimalsDb';
 
-const getRandomAniaml =()=>{
-  const randomIndex = Math.floor(MAth.random()*aniamls.length)
-  return animal[randomIndex].name
-}
+import './App.css';
+import AnimalTable from './AnimalTable';
+
 function App() {
-  const [targetAnimal,setTargetAnimal]= useState(getRandomAnimal)
-  const [result,setResult]=useState('')
-  
-  const handleAnimalClick = (selectAnimal)=>{
-    if(selectAnimal === targetAnimal) {
-        setResult('You Win!')
-        setTargetAnimal(getRandomAnimal())
-  
-    } else {
-        setResult('Try Again!')
-    }
-  }
   return (
-    <div className="game">
-      <h1>Find the Animal: {targetAnimal}</h1>
-      <div className="animal-grid">
-        {animals.map((animal) => (
-          <AnimalCard 
-            key={animal.name} 
-            animal={animal} 
-            onClick={handleAnimalClick} 
-          />
-        ))}
-      </div>
-      <h2>{result}</h2>
+    <div className="App">
+      <AnimalTable/>
     </div>
   );
 }
